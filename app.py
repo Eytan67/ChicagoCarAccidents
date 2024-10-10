c368eddbc9f28929602ce3d4111eaab951d746f6
eytan zichel <eytanzichel@gmail.com>
2024-10-10 09:59:01 +0300

    Creating the basic connection to MongoDB Writing a configuration and connecting it to the application

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from config import Config
+from repository.database import Database
 
 app = Flask(__name__)
 
+
+app.config.from_object(Config)
+
+db = Database(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db.close_connection()
 
 if __name__ == '__main__':
     app.run()
---
+++ repository/database.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from pymongo import MongoClient
+
+class Database:
+    def __init__(self, app: Flask):
+        self.client = MongoClient(app.config['MONGO_URI'])
+        self.db = self.client.my_database1
+
+    def get_collection(self, name):
+        return self.db[name]
+
+    def close_connection(self):
+        if self.client:
+            self.client.close()
